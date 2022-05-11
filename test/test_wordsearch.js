b83f1533e5d410e90b60cc9183b8e85a39693e82
Ali <alihbouran@gmail.com>
2022-05-11 15:29:29 -0700

    Pair programmed word search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,3 +1,4 @@
+
 const chai = require('chai');
 const assert = chai.assert;
 
@@ -34,5 +35,26 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+    
   });
+    it("should return true if the word is present vertically", function() {
+      const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'R', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'K', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+    assert.isTrue(result)
+  })
+
+  it("should return an empty array if the letters is an empty array", function() {
+    const result = wordSearch([], 'FRANK') 
+    assert.isTrue(Array.isArray(result) && result.length === 0);
+  })
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
+// We check if its an empty array
 const wordSearch = (letters, word) => { 
+    if (letters.length === 0) {
+        return [];
+    }
+    
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
-    }
+    } //Checks the array horizontally
+
+    const arr = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        const column = [];
+        for (let j = 0; j < letters.length; j++) {
+            column.push(letters[j][i]);
+        } 
+        arr.push(column);
+    }  
+    // turning columns into rows
+    const verticalJoin = arr.map(ls => ls.join(''))
+    for (let item of verticalJoin) {
+        if (item.includes(word)) return true;
+    }   //looping through the 'columns' to see if there are any matches
+    return false; // if there are no matches either horizontally or vertically, we return false
+
 }
+   
 
 module.exports = wordSearch
